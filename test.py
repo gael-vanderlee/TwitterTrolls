5d7176aff2a7c4496c50205380d32bf356b1a478
Gvanderl <gvanderl@ucsd.edu>
2018-03-10 17:51:10 -0800

    Some progress

--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import pandas as pd
 
 df1 = pd.read_csv('Tweets/Trolls/users.csv')
 df2 = pd.read_csv('Tweets/Trolls/tweets.csv')
-df3 = pd.read_csv('Tweets/Normal_tweets/dashboard_x_usa_x_filter_nativeretweets.csv')
+#df3 = pd.read_csv('Tweets/Normal/coverage_x_usa_x_filter_nativeretweets.csv')
+df4 = pd.read_csv('Tweets/Normal/dashboard_x_usa_x_filter_nativeretweets.csv')
+#df5 = pd.read_csv('Tweets/Normal/geolocation_x_usa_x_filter_nativeretweets.csv')
+#df6 = pd.read_csv('Tweets/Normal/heatmap_x_usa_x_filter_nativeretweets.csv')
 print('done')
