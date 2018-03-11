4349b67213468e377114ff80d01f705fbea47cc9
Gvanderl <gvanderl@ucsd.edu>
2018-03-10 17:22:19 -0800

    Set up template

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
-df1 = pd.read_csv('tweets.csv')
-df2 = pd.read_csv('users.csv')
+df1 = pd.read_csv('Tweets/Trolls/users.csv')
+df2 = pd.read_csv('Tweets/Trolls/tweets.csv')
+df3 = pd.read_csv('Tweets/Normal_tweets/dashboard_x_usa_x_filter_nativeretweets.csv')
 print('done')
